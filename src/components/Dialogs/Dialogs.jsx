d9a5db791f46a6b12916ce01ca697e51d9a8aba2
polufabricatto <dans2704@gmail.com>
2022-01-20 16:12:25 +0300

    take out data arrays from Dialogs and MyPosts to index.js

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,27 +2,17 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import Message from './Message/Message';
 import DialogItem from './DialogItem/DialogItem';
+// import {dialogs} from './../../index';
+// import {messages} from './../../index';
 
 const Dialogs = (props) => {
-	let dialogs = [
-		{ id: 1, name: 'Danon' },
-		{ id: 2, name: 'Danisimo' },
-		{ id: 3, name: 'Dan' },
-		{ id: 4, name: 'Dobian' },
-		{ id: 5, name: 'Dian' },
-	];
+	let dialogsElements = props.dialogs.map((d) => (
+		<DialogItem name={d.name} id={d.id} />
+	));
 
-	let dialogsElements = dialogs.map((d) => (<DialogItem name={d.name} id={d.id} />));
-
-	let messages = [
-		{ id: 1, message: 'Whats up' },
-		{ id: 2, message: 'Hi' },
-		{ id: 3, message: 'Hello' },
-		{ id: 4, message: 'Dorouuuuu' },
-		{ id: 5, message: 'YO' },
-	];
-
-	let messageElements = messages.map((m) => (<Message message={m.message} id={m.id} />));
+	let messageElements = props.messages.map((m) => (
+		<Message message={m.message} id={m.id} />
+	));
 
 	return (
 		<div className={s.dialogs}>
